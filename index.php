d51bac4fda01c5ca821f666f8e21bfb2aa3e0571
kongr45gpen <electrovesta@gmail.com>
2011-07-30 21:42:00 +0300

    Facebook, Google +1

--- index.php
+++ index.php
@@ -3,7 +3,12 @@ ini_set('precision','5'); //Όχι στα μακροσκελή νούμερα!
 header('Content-Type','text/html; charset=utf-8'); //Για να βγαίνουν τα ελληνικά
 ?>
 <!DOCTYPE html>
-<html><head><title>DeltaHacker Facebook Likes</title></head>
+<html><head>
+<title>DeltaHacker Facebook Likes</title>
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js">
+  {lang: 'el'}
+</script>
+</head>
 <body><?php
 
 define('OPEN','placePageStats\\">\\u003cdiv>\\u003cspan class=\\"uiNumberGiant fsxxl fwb\\">'); //Το τμήμα κώδικα ακριβώς πριν τον αριθμό των φαν
@@ -35,4 +40,5 @@ if(!isset($_GET['fans'])) {
 <p>Για να φτάσουμε τους 2^<b><?php echo (int) log($response,2)+1; ?></b> οπαδούς χρειαζόμαστε <?php echo pow(2,(int)log($response,2)+1); ?> άτομα, δηλαδή άλλους <?php echo (int) pow(2,(int)log($response,2)+1)-$response; ?>.</p>
 <p>Ένας εφικτός στόχος είναι οι 2^<?php echo log(2000,2); ?> (2000), δηλαδή άλλα <?php echo 2000-$response ?> άτομα.</p>
 <p>Για τον τελικό στόχο των 2^12.288 (<?php echo pow(2,12.2877); ?>) θα πρέπει να αποκτήσουμε <?php echo pow(2,12.2877)-$response ?> περισσότερα άτομα που θα κάνουν like.</p>
+<p><h2>Δείξτε ενδιαφέρον στο DeltaHacker</h2><iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2FdeltaHacker&amp;send=false&amp;layout=standard&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:25px;" allowTransparency="true"></iframe><g:plusone size="medium" href="http://www.deltahacker.gr/"></g:plusone></p>
 </body></html>
